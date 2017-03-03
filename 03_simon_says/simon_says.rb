ec406f873e07cedfdaeb842bc9d42c6298da251d
Brian <bantunes@my.ccsu.edu>
2017-03-03 12:52:14 -0500

    Completed 03_simon_says test

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,34 @@
 #write your code here
+def echo(word)
+	word
+end
+
+def shout(word)
+	word.upcase
+end
+
+def repeat(word, num=2)
+	("#{word} " * num).strip
+end
+
+def start_of_word(s, num)
+	s.slice(0, num)
+end
+
+def first_word(s)
+	s.split.first
+end
+
+def titleize(num2)
+	num2 = num2.split.map(&:capitalize)
+	
+	for i in 0..num2.length - 1
+		if (num2[i] == 'And') || (num2[i] == 'Over') || (num2[i] == 'The')
+			num2[i] = num2[i].downcase
+		end
+	end
+
+	num2[0] = num2[0].capitalize
+
+return num2.join(' ')
+end
