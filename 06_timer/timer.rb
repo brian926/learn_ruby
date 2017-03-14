4eeab3296d6bb8b3f0aa18e5179499212c5e3ddc
Brian <bantunes@my.ccsu.edu>
2017-03-13 20:32:09 -0400

    Solved Timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,35 @@
 class Timer
   #write your code here
+	attr_accessor :timer, :seconds
+
+	def initialize
+		@seconds=0	
+	end
+
+	def time_string
+		
+		hours=(@seconds/3600)
+		hours_string=hours.to_s
+		if hours_string.length==1
+			hours_string="0"+hours_string
+		end
+
+		minutes=(@seconds%3600)/60
+		minutes_string=minutes.to_s
+		if minutes_string.length==1
+			minutes_string="0"+minutes_string
+		end
+
+		sec=(@seconds%3600)%60
+		sec_string=sec.to_s
+		if sec_string.length==1
+			sec_string="0"+sec_string
+		end
+		
+		return "#{hours_string}:#{minutes_string}:#{sec_string}"
+	end
 end
+
+@timer=Timer.new
+@timer.seconds=6894
+puts @timer.time_string
