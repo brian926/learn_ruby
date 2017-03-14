6fcc8a4e64f59f48be79f4ddcb53af344c774885
Brian <bantunes@my.ccsu.edu>
2017-03-13 20:23:05 -0400

    Solved 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
 class Book
 # write your code here
+	$exceptions=["the", "a", "an", "in", "of", "and"]
+		attr_accessor :title
+
+	def title
+		split_array=@title.split(" ")
+		split_array.map!.with_index do|word, index|
+			if $exceptions.include?(word)&&index!=0
+				word
+			else
+				word.capitalize
+			end
+		end
+		split_array.join(" ")
+	end
 end
+
+@book=Book.new
+@book.title="inferno"
+puts @book.title
